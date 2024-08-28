fcb24772c89e480b7842c0f18b5630ca2ead0fcf
13ruceYu <bruceyuhongbo@gmail.com>
2024-08-28 21:45:59 +0800

    feat: vscode hsl preview

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,87 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+// this method is called when vs code is activated
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "hsl-preview" is now active!');
+	let timeout: NodeJS.Timeout | undefined = undefined;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('hsl-preview.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from hsl-preview!');
-	});
+	// create a decorator type that we use to decorate small numbers
+	const smallNumberDecorationType = vscode.window.createTextEditorDecorationType({});
 
-	context.subscriptions.push(disposable);
+	let activeEditor = vscode.window.activeTextEditor;
+
+  function updateDecorations() {
+    if (!activeEditor || ['css', 'scss', 'less', 'postcss'].indexOf(activeEditor.document.languageId) < 0) {
+      return;
+		}
+
+		const document = activeEditor.document;
+		const decorations: vscode.DecorationOptions[] = [];
+
+		for (let i = 0; i < document.lineCount; i++) {
+			const line = document.lineAt(i);
+			const regex = /(?:hsl\(|(--[\w-]+:\s*))?([\d.]+)\s+([\d.]+)%\s+([\d.]+)%/g;
+      const matches = line.text.matchAll(regex);
+			
+			for (const match of matches) {
+				const [fullMatch, , h, s, l] = match;
+            
+        // Calculate the start position of the color values
+        const valueStartIndex = match.index! + (fullMatch.length - `${h} ${s}% ${l}%`.length);
+        const startPos = new vscode.Position(i, valueStartIndex);
+        const endPos = new vscode.Position(i, valueStartIndex + `${h} ${s}% ${l}%`.length);
+        const range = new vscode.Range(startPos, endPos);
+
+				const hslColor = `hsl(${h}, ${s}%, ${l}%)`;
+				const decorationType = vscode.window.createTextEditorDecorationType({
+					backgroundColor: hslColor,
+          borderRadius: '2px',
+          color: Number(l) > 50 ? 'black' : 'white',
+				});
+
+				activeEditor.setDecorations(decorationType, [{ range }]);
+				decorations.push({ range });
+			}
+		}
+
+		// Apply hover decorations
+		activeEditor.setDecorations(smallNumberDecorationType, decorations);
+	}
+
+	function triggerUpdateDecorations(throttle = false) {
+		if (timeout) {
+			clearTimeout(timeout);
+			timeout = undefined;
+		}
+		if (throttle) {
+			timeout = setTimeout(updateDecorations, 500);
+		} else {
+			updateDecorations();
+		}
+	}
+
+	if (activeEditor) {
+		triggerUpdateDecorations();
+	}
+
+	vscode.window.onDidChangeActiveTextEditor(
+		(editor) => {
+			activeEditor = editor;
+			if (editor) {
+				triggerUpdateDecorations();
+			}
+		},
+		null,
+		context.subscriptions
+	);
+
+	vscode.workspace.onDidChangeTextDocument(
+		(event) => {
+			if (activeEditor && event.document === activeEditor.document) {
+				triggerUpdateDecorations(true);
+			}
+		},
+		null,
+		context.subscriptions
+	);
 }
-
-// This method is called when your extension is deactivated
-export function deactivate() {}
