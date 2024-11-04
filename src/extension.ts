5edd6fc0dc29f089bb93b6da2e7827f06fff214b
13ruceYu <bruceyuhongbo@gmail.com>
2024-11-04 11:21:58 +0800

    fix: tailwindcss language mode color display

--- src/extension.ts
+++ src/extension.ts
@@ -10,7 +10,7 @@ export function activate(context: vscode.ExtensionContext) {
   let activeEditor = vscode.window.activeTextEditor
 
   function updateDecorations() {
-    if (!activeEditor || !['css', 'scss', 'less', 'postcss'].includes(activeEditor.document.languageId)) {
+    if (!activeEditor || !['css', 'scss', 'less', 'postcss', 'tailwindcss'].includes(activeEditor.document.languageId)) {
       return
     }
 
